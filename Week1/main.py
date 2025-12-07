78b28d8ca71f4053fb8f904905f9dec9b9319356
jchen <jchen@cvc.uab.cat>
2025-12-07 18:42:23 +0100

    added metrics

--- Week1/main.py
+++ Week1/main.py
@@ -6,9 +6,10 @@ from typing import List, Literal, Tuple, Type
 import numpy as np
 import tqdm
 from bovw import BOVW
+from natsort import natsorted
 from PIL import Image
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import auc, f1_score, precision_score, recall_score, roc_curve
 
 
 def extract_bovw_histograms(bovw: Type[BOVW], descriptors: Literal["N", "T", "d"]):
@@ -22,10 +23,42 @@ def extract_bovw_histograms(bovw: Type[BOVW], descriptors: Literal["N", "T", "d"
     )
 
 
+def compute_metrics(y_true: List[int], y_pred: List[int], map_classes: dict):
+    precision = precision_score(y_true, y_pred, average="weighted")
+    recall = recall_score(y_true, y_pred, average="weighted")
+    f1 = f1_score(y_true, y_pred, average="weighted")
+
+    n_classes = len(map_classes)
+    false_positive_rates = []
+    true_positive_rates = []
+    auc_scores = []
+
+    for i in range(n_classes):
+        y_mask = [1 if label == i else 0 for label in y_true]
+        y_pred_mask = [1 if label == i else 0 for label in y_pred]
+        false_positive_rates[i], true_positive_rates[i], _ = roc_curve(
+            y_mask, y_pred_mask
+        )
+        auc_scores.append(auc(false_positive_rates[i], true_positive_rates[i]))
+    auc_average = np.sum(auc_scores) / n_classes
+
+    return {
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+        "num_classes": n_classes,
+        "false_positive_rates": false_positive_rates,
+        "true_positive_rates": true_positive_rates,
+        "auc_scores": auc_scores,
+        "auc_average": auc_average,
+    }
+
+
 def test(
     dataset: List[Tuple[Type[Image.Image], int]],
     bovw: Type[BOVW],
     classifier: Type[object],
+    map_classes: dict,
     batch_size: int = 50,
 ):
     all_histograms = []
@@ -57,14 +90,26 @@ def test(
     print("predicting the values")
     y_pred = classifier.predict(all_histograms)
 
-    print(
-        "Accuracy on Phase[Test]:",
-        accuracy_score(y_true=all_labels, y_pred=y_pred),
-    )
+    output = compute_metrics(all_labels, y_pred, map_classes=map_classes)
+
+    aux_auc_per_class = {}
+    for cls_name, cls_idx in map_classes.items():
+        aux_auc_per_class[cls_name] = output["auc_scores"][cls_idx]
+    print("Metrics on Phase[Eval]:")
+    print(f" - Precision: {output['precision']}")
+    print(f" - Recall: {output['recall']}")
+    print(f" - F1 Score: {output['f1_score']}")
+    print(f" - AUC: {output['auc_average']}")
+    print(f" - AUC per class: {aux_auc_per_class}")
+
+    return output
 
 
 def train(
-    dataset: List[Tuple[Type[Image.Image], int]], bovw: Type[BOVW], batch_size: int = 50
+    dataset: List[Tuple[Type[Image.Image], int]],
+    bovw: Type[BOVW],
+    map_classes: dict,
+    batch_size: int = 50,
 ):
     # Phase 1: Fit Codebook progressively
     print("Phase [Training]: Fitting Codebook progressively")
@@ -113,16 +158,26 @@ def train(
         all_histograms, all_labels
     )
 
-    print(
-        "Accuracy on Phase[Train]:",
-        accuracy_score(y_true=all_labels, y_pred=classifier.predict(all_histograms)),
+    output = compute_metrics(
+        all_labels, classifier.predict(all_histograms), map_classes=map_classes
     )
+
+    auc_per_class = {}
+    for cls_name, cls_idx in map_classes.items():
+        auc_per_class[cls_name] = output["auc_scores"][cls_idx]
+    print("Metrics on Phase[Train]:")
+    print(f" - Precision: {output['precision']}")
+    print(f" - Recall: {output['recall']}")
+    print(f" - F1 Score: {output['f1_score']}")
+    print(f" - AUC: {output['auc_average']}")
+    print(f" - AUC per class: {auc_per_class}")
 
     return bovw, classifier
 
 
 def Dataset(
     ImageFolder: str = "data/MIT_split/train",
+    map_classes: dict = {},
 ) -> List[Tuple[Type[Image.Image], int]]:
     """
     Expected Structure:
@@ -138,12 +193,14 @@ def Dataset(
             ImageFolder/cat/[...]/asd932_.png
 
     """
-
-    map_classes = {clsi: idx for idx, clsi in enumerate(os.listdir(ImageFolder))}
+    if not map_classes:
+        map_classes = {
+            clsi: idx for idx, clsi in enumerate(natsorted(os.listdir(ImageFolder)))
+        }
 
     dataset: List[Tuple] = []
 
-    for idx, cls_folder in enumerate(os.listdir(ImageFolder)):
+    for idx, cls_folder in enumerate(natsorted(os.listdir(ImageFolder))):
         image_path = os.path.join(ImageFolder, cls_folder)
         images: List[str] = glob.glob(image_path + "/*.jpg")
         for img in images:
@@ -151,16 +208,20 @@ def Dataset(
 
             dataset.append((img_pil, map_classes[cls_folder], img))
 
-    return dataset
+    return dataset, map_classes
 
 
 if __name__ == "__main__":
     # /home/cboned/data/Master/MIT_split
-    data_train = Dataset(ImageFolder="./data/train")
-    data_test = Dataset(ImageFolder="./data/val")
+    data_train, map_classes = Dataset(ImageFolder="./data/train")
+    data_test, _ = Dataset(ImageFolder="./data/val", map_classes=map_classes)
 
-    bovw = BOVW(detector_type="SIFT")
+    codebook_sizes = [50, 100, 200, 400, 800, 1600, 3200]
 
-    bovw, classifier = train(dataset=data_train, bovw=bovw)
+    for codebook_size in codebook_sizes:
+        print(f"\n\nTraining with codebook size: {codebook_size}")
+        bovw = BOVW(detector_type="SIFT", codebook_size=codebook_size)
 
-    test(dataset=data_test, bovw=bovw, classifier=classifier)
+        bovw, classifier = train(dataset=data_train, bovw=bovw, batch_size=100)
+
+        test(dataset=data_test, bovw=bovw, classifier=classifier, batch_size=100)
